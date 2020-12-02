1cea4bab72f77dd32ea8536dad3f7a729e426c19
chickenbror <75183404+chickenbror@users.noreply.github.com>
2020-12-02 19:30:07 +0100

    Updated exceptions.py
    Updated exceptions testing according to Canvas feedback:
    Defined test() to raise error within function instead of globally, so the error can be caught by try-except in the catch_something() functions.

--- tasks/exceptions.py
+++ tasks/exceptions.py
@@ -5,37 +5,45 @@ fill in the functions so that they catch the appropriate errors and return "caug
 
 example:
 >> test_list = []
->> catch_all_expeptions(test_list[10])
+>> catch_all_exceptions(test_list[10])
 >> "caught"
 
 """
 
-
-def catch_all_expeptions():
-    pass
-
-####### It works if I write them globally but I don't know how to put them in a function :(
-test_list = []
-try:
-    test_list[10]
-except:
-    print('caught')
+#######
+def catch_all_exceptions(func):
+    try:
+        func()
+    except: #Catches any error
+        print('caught')
 
 
-def catch_index_error():
-    pass
-try:
-    test_list[10]
-except IndexError as err:
-    print('caught', err)
+def catch_index_error(func):
+    try:
+        func()
+    except IndexError as err:
+        print('caught:', err)
 
-def catch_value_error():
-    pass
-try:
-    int('haha')
-except ValueError as err:
-    print('caught', err)
+def catch_value_error(func):
+    try:
+        func()
+    except ValueError as err:
+        print('caught:', err)
+#========================================
+def test():
+    l = [1,2,3]
+    print(l[1]) #in range
+    print(l[10]) #index out of range
+
+    
+catch_all_exceptions(test)
+catch_index_error(test)
 
 
 
+def value_err():
+    print(int(1.2345))
+    print(int('hello'))
+
+catch_value_error(value_err)
  
