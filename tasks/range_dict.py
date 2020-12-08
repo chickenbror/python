29ee9152a5061e79abd2e99573538c2dc1b31c37
Sigrid J <27883387+si-jon@users.noreply.github.com>
2020-12-08 15:02:44 +0100

    Update range_dict.py
    Updated range_dict: checks and returns if item is in range.

--- tasks/range_dict.py
+++ tasks/range_dict.py
@@ -21,4 +21,27 @@ trying to add (5,11) when we have (1,10), overwrite the overlapping span.
 >> my_range_dict
 >> {(30,34): 2, (5,11):3}
 
-""""
+"""
+
+class RangeDict(dict):
+  
+    def __getitem__(self, arg):
+        for key in self.keys():
+            if key[0] <= arg <= key[1]:
+                return key
+    
+    def __setitem__(self, key, item):
+        key_to_delete = None
+        for existing_key in self.keys():
+            if existing_key[0] <= key[0] <= existing_key[1] \
+                or existing_key[0] <= key[1] <= existing_key[1]:
+                key_to_delete = existing_key
+        
+        if key_to_delete:
+            self.pop(key_to_delete)
+        super().__setitem__(key, item)
+        
+
+my_range_dict = RangeDict({(1,10):1, (30,34): 2, (5,10):5})
+my_range_dict[(25,29)] = 3
+my_range_dict[(5,15)] = 3
